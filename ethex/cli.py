9b79299c8ce72b51a7346679c37b419e1f403bf3
Alan Höng <alan.f.hoeng@gmail.com>
2018-05-19 13:25:55 +0200

    move preprocess command to new cli interface module

--- ethex/cli.py
+++ ethex/cli.py
@@ -1,33 +1,40 @@
 """Pull data from geth and parse it into mongo."""
-
+import logging
 import subprocess
-import sys
-sys.path.append("./../Preprocessing")
-sys.path.append("./../Analysis")
-import os
-os.environ['ETH_BLOCKCHAIN_ANALYSIS_DIR'] = './../Preprocessing/'
-from Crawler import Crawler
-from ContractMap import ContractMap
-import subprocess
-import time
-LOGDIR = "./../Preprocessing/logs"
 
+import click
+
+from ethex.analysis.contract_map import ContractMap
+from ethex.crawler.crawler import Crawler
+from ethex.structure import PATH
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 def preprocess():
     subprocess.call([
-        "(geth --rpc --rpcport 8545 > {}/geth.log 2>&1) &".format(LOGDIR),
-        "(mongod --dbpath mongo/data --port 27017 > {}/mongo.log 2>&1) &".format(LOGDIR)
+        "(geth --rpc --rpcport 8545 > {}/geth.log 2>&1) &".format(PATH['LOGDIR']),
+        "(mongod --dbpath mongo/data --port 27017 > {}/mongo.log 2>&1) &"
+            .format(PATH['LOGDIR'])
     ], shell=True)
 
-    print("Booting processes.")
+    logging.info("Booting processes.")
     # Catch up with the crawler
     c = Crawler()
 
-    print("Updating contract hash map.")
+    logging.info("Updating contract hash map.")
     # Update the contract addresses that have been interacted with
     ContractMap(c.mongo_client, last_block=c.max_block_mongo)
 
-    print("Update complete.")
+    logging.info("Update complete.")
     subprocess.call([
         "(geth --rpc --rpcport 8545 > {}/geth.log 2>&1) &".format(LOGDIR),
         "(mongod --dbpath mongo/data --port 27017 > {}/mongo.log 2>&1) &".format(LOGDIR)
     ], shell=True)
+
+
+if __name__ == '__main__':
+    cli()
